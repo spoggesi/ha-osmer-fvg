9ccbc350d671a693c07ababdd350cbd2309ba3e9
Simone Poggesi <s.poggesi@giottoconsulting.it>
2026-07-30 15:43:08 +0000

    Add Home Assistant sensor entities

--- custom_components/osmer_fvg/sensor.py
+++ custom_components/osmer_fvg/sensor.py
@@ -5,7 +5,9 @@ from __future__ import annotations
 from typing import Any
 
 from homeassistant.components.sensor import (
+    SensorDeviceClass,
     SensorEntity,
+    SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -14,7 +16,9 @@ from homeassistant.helpers.entity_platform import (
 )
 
 from .const import MONITORED_SENSORS
-from .coordinator import OsmerDataUpdateCoordinator
+from .coordinator import (
+    OsmerDataUpdateCoordinator,
+)
 
 
 async def async_setup_entry(
@@ -29,19 +33,21 @@ async def async_setup_entry(
     )
 
 
-    entities = []
+    entities: list[SensorEntity] = []
 
 
-    for code in MONITORED_SENSORS:
+    for code in coordinator.data.sensors:
 
-        if code in coordinator.data.sensors:
+        if code not in MONITORED_SENSORS:
+            continue
 
-            entities.append(
-                OsmerSensor(
-                    coordinator,
-                    code,
-                )
+
+        entities.append(
+            OsmerSensor(
+                coordinator,
+                code,
             )
+        )
 
 
     async_add_entities(
@@ -49,8 +55,9 @@ async def async_setup_entry(
     )
 
 
+
 class OsmerSensor(
-    SensorEntity
+    SensorEntity,
 ):
     """Representation of an OSMER sensor."""
 
@@ -69,23 +76,23 @@ class OsmerSensor(
         self.sensor_code = sensor_code
 
 
-        sensor = coordinator.data.sensors[
-            sensor_code
-        ]
+        sensor = (
+            coordinator.data.sensors[sensor_code]
+        )
 
 
-        self._sensor = sensor
+        station = (
+            coordinator.data.station
+        )
 
 
         self._attr_unique_id = (
-            f"osmer_{sensor.station_id}_{sensor.code}"
+            f"osmer_{station.id}_{sensor.code}"
         )
 
 
         self._attr_name = (
-            MONITORED_SENSORS[
-                sensor_code
-            ]["name"]
+            MONITORED_SENSORS[sensor.code]["name"]
         )
 
 
@@ -94,20 +101,22 @@ class OsmerSensor(
         )
 
 
-        device_class = (
-            MONITORED_SENSORS[
-                sensor_code
-            ].get(
-                "device_class"
+        self._attr_device_class = (
+            self._get_device_class(
+                sensor.code
             )
         )
 
 
-        if device_class:
+        self._attr_state_class = (
+            SensorStateClass.MEASUREMENT
+        )
 
-            self._attr_device_class = (
-                device_class
-            )
+
+        self._attr_icon = (
+            "mdi:weather-partly-cloudy"
+        )
+
 
 
     @property
@@ -142,44 +151,50 @@ class OsmerSensor(
         )
 
 
-        return {
-            "station": station.name,
-            "station_id": station.id,
-            "latitude": station.latitude,
-            "longitude": station.longitude,
-            "altitude": station.altitude,
-            "sensor_code": self.sensor_code,
-            "sensor_name": self._sensor.name,
-        }
-
-
-
-    @property
-    def device_info(self):
-        """Return device information."""
-
-        station = (
-            self.coordinator.data.station
+        sensor = (
+            self.coordinator.data.sensors[
+                self.sensor_code
+            ]
         )
 
 
         return {
-            "identifiers": {
-                (
-                    "osmer_fvg",
-                    str(station.id),
-                )
-            },
-            "name": (
-                f"OSMER {station.name}"
-            ),
-            "manufacturer": (
-                "Protezione Civile FVG"
-            ),
-            "model": (
-                "Weather Station"
-            ),
-            "sw_version": (
-                station.status
+            "station": station.name,
+            "station_id": station.id,
+            "sensor_code": sensor.code,
+            "sensor_name": sensor.name,
+            "latitude": station.latitude,
+            "longitude": station.longitude,
+            "altitude": station.altitude,
+            "last_update": (
+                self.coordinator.data.measures[
+                    self.sensor_code
+                ].timestamp.isoformat()
+                if self.sensor_code
+                in self.coordinator.data.measures
+                else None
             ),
         }
+
+
+
+    @staticmethod
+    def _get_device_class(
+        code: str,
+    ) -> SensorDeviceClass | None:
+        """Return HA device class."""
+
+        mapping = {
+
+            "T": SensorDeviceClass.TEMPERATURE,
+
+            "U": SensorDeviceClass.HUMIDITY,
+
+            "RR": SensorDeviceClass.PRECIPITATION_INTENSITY,
+
+            "P": SensorDeviceClass.PRECIPITATION,
+
+        }
+
+
+        return mapping.get(code)
